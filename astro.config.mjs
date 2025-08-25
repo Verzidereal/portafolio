57bfabd0f39875b411fbbf44a6b8f2df46f4f800
Verzidereal <minquizreal@gmail.com>
2025-08-24 23:37:47 -0600

    Actualizar astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import image from '@astrojs/image';
 
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://verzide.com', // 👈 importante para sitemap y SEO
+  integrations: [
+    react(),
+    tailwind(),
+    sitemap(),
+    image(),
+  ],
   vite: {
-    server: { port: 4321 }
-  }
+    server: { port: 4321 },
+  },
 });
