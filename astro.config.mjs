1f5ac19d4239c14a5cd019a65888cf7133ff48bf
Verzidereal <minquizreal@gmail.com>
2025-08-24 23:45:27 -0600

    Actualizar astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
 
